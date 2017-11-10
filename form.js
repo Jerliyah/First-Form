33ef4712af03d168cb4139d85b857e2a4af6c7b5
Jerliyah <jerliyahcraig@gmail.com>
2017-11-10 15:14:57 -0600

    changing direction with the js

--- form.js
+++ form.js
@@ -1,73 +1,27 @@
-// All the p tags will be collected into an array called options
-let options = document.querySelectorAll('p');
 
 
+let options = document.querySelectorAll('p'); // Returns NodeList
 
-// A function to get siblings of a given element
-function getSiblings(element) {
-    // Look at the element's parents and grab all of its children
-    let allChildren = element.parentElement.children;
-
-    // Convert allChildren to an array
-    allChildrenList = objectToArray(allChildren);
-
-    // Remove the given element from the children list so that we are left with just the siblings
-    let elementIndex = allChildrenList.indexOf(element);
-    let siblings = allChildrenList.filter( (child) => { return child != element });
-
-    // return siblings
-    return siblings;
-}
-
-
-function objectToArray(object) {
-    array = [];
-    
-    for(let i=0; i < object.length; i++) {
-        array.push(object[i]);
-    }
-
-    return array;
-}
-
-
-function hasSelectedClass(element) {
-    if ( element.classList.contains('selected') ) {
-        return element
-    }
-}
-
-
-
-
-// For each item in the options array (p elements)...
 options.forEach( (element) => {
 
-    // Add a listener for when the mouse hover over it...
-    element.addEventListener( 'click', () => {
-        // Get the siblings of the element
-        let siblings = getSiblings(element);
-
-        // Remove 'selected' class from any previously hovered on items
-        siblings.forEach( (sibling) => {
-            if ( sibling.classList.contains('selected') ) {
-                sibling.classList.remove('selected');
-            }
-        });
-
-        // Add the selected class to this specific element
-        element.classList.add('selected');
-    });
+    element.addEventListener( 'click', () => { there_can_be_only_one_selected(element) } );
+        
 });
 
 
 
-// Create a variable for the button
 let button = document.querySelector('button');
+let questionItems = object_to_array( document.querySelectorAll('li') ); 
+let questions = [];
 
-// When the button is clicked...
+// Get raw strings of question instead of elements
+questionItems.forEach( (questionItem) => {
+                questions.push( questionItem.innerText );
+})
+
+
 button.addEventListener('click', () => {
-    // Get all of the selected element...
+    // Get all of the selected elements...
     let selectedOptions = document.querySelectorAll('.selected');
     let selectedInput = [];
 
@@ -82,3 +36,52 @@ button.addEventListener('click', () => {
     // TODO: Autodirect to a new page personalized due to their selection
 });
 
+
+
+
+
+
+// Helper Functions
+function get_siblings(element) {
+    let allChildren = element.parentElement.children; // Returns NodeList
+
+    allChildrenList = object_to_array(allChildren);
+
+    // Remove the given element from the children list so that we are left with just the siblings
+    let elementIndex = allChildrenList.indexOf(element);
+    let siblings = allChildrenList.filter( (child) => { return child != element });
+
+    return siblings;
+}
+
+
+function has_selected_class(element) {
+    if ( element.classList.contains('selected') ) {
+        return element
+    }
+}
+
+
+function object_to_array(object) {
+    array = [];
+    
+    for(let i=0; i < object.length; i++) {
+        array.push(object[i]);
+    }
+
+    return array;
+}
+
+
+function there_can_be_only_one_selected(element) {
+    let siblings = get_siblings(element);
+    
+    siblings.forEach( (sibling) => {
+        if ( sibling.classList.contains('selected') ) {
+            sibling.classList.remove('selected');
+        }
+    });
+
+    element.classList.add('selected');
+
+}
